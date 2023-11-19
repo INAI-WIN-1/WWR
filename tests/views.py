3b48ae1b62a9e14fe7ab37d23ba011ce5e0dc7dd
lepri4dw <lepri4dw@gmail.com>
2023-11-19 22:39:51 +0600

    Made logic, that questions don't repeat and added a modal for finishing the game

--- tests/views.py
+++ tests/views.py
@@ -11,12 +11,28 @@ def indexView(request):
 def QuestionsView(request, level, id):
     if request.method == "GET":
         tests = Test.objects.filter(level=level.upper())
-        abc = 'ABCD'
-        for test in tests:
-            test.answers = test.answers.split(', ')
-        return render(request, 'tests.html', {'test': random.choice(tests), 'id': id, 'abc': abc})
+        asked_questions = request.session.get('asked_questions', [])
+        print(asked_questions)
+
+        available_tests = [test for test in tests if test.id not in asked_questions]
+
+        if not available_tests:
+            request.session['asked_questions'] = []
+            return render(request, 'tests.html', {'finish_game': True, 'id': id - 1})
+
+        test = random.choice(available_tests)
+        test.answers = test.answers.split(', ')
+        asked_questions.append(test.id)
+        request.session['asked_questions'] = asked_questions
+        return render(request, 'tests.html', {'test': test, 'id': id})
 
     elif request.method == "POST":
+        finish_game = request.POST.get('finish_game')
+        print(finish_game)
+        if finish_game:
+            request.session['asked_questions'] = []
+            # здесь будет логика зачисления денег на баланс пользователя
+            return render(request, 'tests.html', {'finish_game': True, 'id': id - 1})
         selected_test = request.POST.get('selected_test')
         selected_answer, correct_answer = selected_test.split('|')
 
@@ -24,4 +40,5 @@ def QuestionsView(request, level, id):
             next_id = id + 1
             return redirect('questions', level=level, id=next_id)
         else:
+            request.session['asked_questions'] = []
             return render(request, 'tests.html', {'level': level, 'id': id, 'wrong_answer': True})
