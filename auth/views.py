32e4281a15110c4a8b533e8868d383e05e5822c6
User <imanbek.akylbekov2017@gmail.com>
2023-11-25 02:07:56 +0600

    finished functionality of auth, done sign in and sign up with redirects without layout

--- auth/views.py
+++ auth/views.py
@@ -5,8 +5,17 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from auth.forms import UserSignUpForm
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 class UserCreateView(CreateView):
     form_class = UserSignUpForm
     success_url = reverse_lazy('main')
     template_name ='authLayOut/registr.html'
+
+
+
+class CustomLoginView(LoginView):
+    template_name = 'authLayOut/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('main')
