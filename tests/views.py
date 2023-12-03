d4f2e06521e4b74cbc546042a0eceaef771690d6
lepri4dw <lepri4dw@gmail.com>
2023-12-03 15:11:31 +0600

    Added rating's logic to tests, translated tests page

--- tests/views.py
+++ tests/views.py
@@ -8,13 +8,23 @@ def indexView(request):
         return render(request, template_name='index.html', context={'posts': 'hello from tests'})
 
 
+ratings_by_level = {
+    'EASY': [500, 1000, 2000, 3000, 5000, 10000, 15000, 25000, 50000, 100000],
+    'MEDIUM': [5000, 10000, 15000, 25000, 50000, 100000, 150000, 200000, 300000, 400000],
+    'HARD': [50000, 100000, 150000, 200000, 300000, 400000, 600000, 800000, 1000000, 3000000]
+}
+
+
 def QuestionsView(request, level, id):
+    level = level.upper()
+    ratings = ratings_by_level[level]
+    index = id - 1
+    rating = ratings[id - 1]
     if request.method == "GET":
         chosen_tests = request.session.get('chosen_tests', [])
-        print(chosen_tests)
-
+    
         if not chosen_tests:
-            tests = list(Test.objects.filter(level=level.upper()))
+            tests = list(Test.objects.filter(level=level))
 
             if len(tests) >= 10:
                 selected_tests = random.sample(tests, 10)
@@ -24,21 +34,19 @@ def QuestionsView(request, level, id):
             chosen_tests = [{'id': test.id, 'question': test.question, 'answers': test.answers.split(', '), 'correct_answer': test.correct_answer} for test in selected_tests]
             request.session['chosen_tests'] = chosen_tests
 
-        # available_tests = [test for test in tests if test.id not in chosen_tests]
-        #
         if len(chosen_tests) < id:
             request.session['chosen_tests'] = []
-            return render(request, 'tests.html', {'finish_game': True, 'id': id - 1})
+            return render(request, 'tests.html', {'finish_game': True, 'id': index, 'rating': rating})
         test = chosen_tests[id-1]
 
-        return render(request, 'tests.html', {'test': test, 'id': id})
+        return render(request, 'tests.html', {'test': test, 'id': id, 'rating': rating})
 
     elif request.method == "POST":
         finish_game = request.POST.get('finish_game')
         if finish_game:
             request.session['chosen_tests'] = []
             # здесь будет логика зачисления денег на баланс пользователя
-            return render(request, 'tests.html', {'finish_game': True, 'id': id - 1})
+            return render(request, 'tests.html', {'finish_game': True, 'id': index, 'rating': ratings[id - 2]})
         selected_test = request.POST.get('selected_test')
         selected_answer, correct_answer = selected_test.split('|')
 
@@ -47,4 +55,4 @@ def QuestionsView(request, level, id):
             return redirect('questions', level=level, id=next_id)
         else:
             request.session['chosen_tests'] = []
-            return render(request, 'tests.html', {'level': level, 'id': id, 'wrong_answer': True})
+            return render(request, 'tests.html', {'level': level, 'id': id, 'wrong_answer': True, 'rating': rating})
