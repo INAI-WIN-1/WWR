736840d9a557ea9eb4e25f747b17ec7002a94929
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-12-11 22:31:57 +0600

    adding changes from css

--- auth/views.py
+++ auth/views.py
@@ -6,6 +6,18 @@ from django.views.generic import CreateView
 from auth.forms import UserSignUpForm
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+
+
+def my_view(request):
+    print(request.session)
+    user_data = {
+        'username': request.user.username,
+        'email': request.user.email,
+        'is_authenticated': request.user.is_authenticated,
+    }
+    return render(request, 'index.html', {'user_data': user_data})
+
 
 class UserCreateView(CreateView):
     form_class = UserSignUpForm
