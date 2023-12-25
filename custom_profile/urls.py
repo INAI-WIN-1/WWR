f71950fcc7e87113406f174e2fb7ff41b48f0bec
lepri4dw <lepri4dw@gmail.com>
2023-12-26 01:53:30 +0600

    Added avatar to profile model

--- custom_profile/urls.py
+++ custom_profile/urls.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
-from custom_profile.views import profile_view
+from custom_profile.views import profile_view, ProfileUpdateView
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
+    path('profile/<int:id>/update/', ProfileUpdateView.as_view(), name='profile_update')
 ]
